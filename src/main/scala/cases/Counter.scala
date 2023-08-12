4bb4d7eba5a2bade4371f3e2ea6a4c0d1a9988c9
xiangmufan <xiangmufan@bytedance.com>
2023-08-12 17:05:41 +0800

    :sparkles: add converting from chisel to btor2

--- src/main/scala/cases/Counter.scala
+++ src/main/scala/cases/Counter.scala
@@ -16,10 +16,11 @@ class Counter(max_val: Int) extends Module with Formal {
   assert(count < 11.U)
 
   assertAt(0.U, count === 0.U) // when reset, count is 0
-  assertAt(1.U, count === 1.U) // after 1 cycle, count is 1
-  assertAt(2.U, count === 2.U) // after 2 cycles, count is 2
-  assertAt(3.U, count === 3.U) // after 3 cycles, count is 3
-  assertAt(4.U, count === 4.U) // after 4 cycles, count is 4
+  assertAt(1.U, count === 1.U) // at cycle 1, count is 1
+  assertAt(2.U, count === 2.U) // at cycle 2, count is 2
+  assertAt(3.U, count === 3.U) // at cycle 3, count is 3
+  assertAt(4.U, count === 4.U) // at cycle 4, count is 4
+  assertAt((max_val + 1).U, count === 0.U) // at cycle max_val + 1, count is 0
   //  past(count, 1) { lastCount =>
   //    when(lastCount =/= max_val.U) {
   //      assert(lastCount + 1.U === count)
